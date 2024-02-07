d4d08822680fe648dd068147054d91008e0f62fc
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-02-07 11:11:54 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import {
   Button,
   SafeAreaView,
   StatusBar,
+  StyleSheet,
   Text,
   View,
 } from "react-native";
+
 const vai = () => {
   Alert.alert("Titulo da janela", "Fala parça!");
 };
@@ -14,14 +16,14 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <SafeAreaView>
-        <View>
-          <Text>Topo/cabecalho</Text>
+      <SafeAreaView style={estilos.container}>
+        <View style={estilos.cabecalho}>
+          <Text style={estilos.texto}>Topo/cabecalho</Text>
         </View>
-        <View>
+        <View style={estilos.conteudo}>
           <Text>Conteúdo...</Text>
         </View>
-        <View>
+        <View style={estilos.rodape}>
           <Text>Rodapé...</Text>
           <Button title="Botão" onPress={vai} />
         </View>
@@ -29,3 +31,27 @@ export default function App() {
     </>
   );
 }
+
+const estilos = StyleSheet.create({
+  container: {
+    backgroundColor: "lightblue",
+    flex: 1,
+    // justifyContent: "center",
+    // flexDirection: "row",
+  },
+
+  cabecalho: {
+    backgroundColor: "lightgreen",
+    flex: 1,
+  },
+
+  conteudo: {
+    backgroundColor: "gray",
+    flex: 4,
+  },
+
+  rodape: {
+    backgroundColor: "orange",
+    flex: 1,
+  },
+});
